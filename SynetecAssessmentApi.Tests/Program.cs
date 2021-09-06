95a0c94ed9ae0ecdcc1bcac32a3a44cae18f27b8
Eduard Myronenko <e.o.myronenmo@gmail.com>
2021-09-06 13:43:26 +0300

    fix db

---
+++ SynetecAssessmentApi.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SynetecAssessmentApi.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
